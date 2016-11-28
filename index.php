50126e6a9e8afa8bcbddab8e8c3154cb88fa7f4b
Simon Mitchell <kartano@gmail.com>
2016-11-28 08:13:31 -0600

    Added header, removed blank lines.
    Just housekeeping while reading code!

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+/**
+ * Description
+ *
+ * @author          Andrew Jeffries <andrew.jeffries@sunsetcoders.com.au>
+ * @version         1.0.0               2016-11-28 08:12:35 SM:  Prototype
+ */
 
 include ('auth.php');
 include ('function_class.php');
-
-
-
 sunlibrary();
-
+?>
